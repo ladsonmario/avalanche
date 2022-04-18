a88b5965974108b1e308853aa61951911e0cf5ce
Ladson Mario <ladson.mario@hotmail.com>
2022-04-18 10:55:05 -0300

    uploader dos arquivos para o projeto avalanche

---
+++ assets/js/script.js
@@ -0,0 +1,37 @@
+const menuItems = document.querySelectorAll('.header-options a[href^="#"]');
+
+function getScrollTopByHref(element) {
+	const id = element.getAttribute('href');
+	return document.querySelector(id).offsetTop;
+}
+
+function scrollToPosition(to) {    
+    window.scroll({
+        top: to,
+        behavior: "smooth",
+    });  
+}
+
+function scrollToIdOnClick(event) {
+	event.preventDefault();
+	const to = getScrollTopByHref(event.currentTarget)- 80;
+	scrollToPosition(to);
+}
+
+menuItems.forEach(item => {
+	item.addEventListener('click', scrollToIdOnClick);
+});
+
+document.querySelector('.menu--mobile').addEventListener('click', ()=>{       
+    document.querySelector('.header-options ul').style.right = '0px';
+});
+
+document.querySelector('.menu-closer').addEventListener('click', ()=>{
+    document.querySelector('.header-options ul').style.right = '-700px';    
+});
+
+document.querySelectorAll('nav a').forEach(e => {
+	e.addEventListener('click', ()=> {
+		document.querySelector('.header-options ul').style.right = '-700px';
+	});
+});
